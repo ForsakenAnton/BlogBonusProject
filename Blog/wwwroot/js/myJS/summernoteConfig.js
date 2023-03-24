684b2b3a1273a2d60b3586e546a29f8d934a6ba5
Anton <forsakenanton@gmail.com>
2023-03-24 13:19:35 +0200

    Add summernote fix bags

--- Blog/wwwroot/js/myJS/summernoteConfig.js
+++ Blog/wwwroot/js/myJS/summernoteConfig.js
@@ -1,2 +1,15 @@
 ﻿$('#summernote').summernote({
+    toolbar: [
+        ['style', ['style']],
+        //['style', ['bold', 'italic', 'underline', 'clear']],
+        ['font', ['bold', 'underline', 'clear', 'strikethrough', 'superscript', 'subscript']],
+        ['fontsize', ['fontsize']],
+        ['fontname', ['fontname']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['height', ['height']]
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['fullscreen', 'codeview', 'help']],
+    ]
 });
